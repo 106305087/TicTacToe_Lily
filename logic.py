a1fe2c4953524dfe39813edbd8d782e9fcc9ff25
Lily Chou <43545102+106305087@users.noreply.github.com>
2023-11-07 18:26:08 -0800

    Update logic.py

--- logic.py
+++ logic.py
@@ -12,11 +12,36 @@ def make_empty_board():
 
 
 def get_winner(board):
-    """Determines the winner of the given board.
-    Returns 'X', 'O', or None."""
-    return None  # FIXME
+  #check row
+  for row in board:
+    if len(set(row)) == 1 and row[0]!='':
+      return row[0]
+
+  #check columns
+  for i in range(len(board)):
+    column = [board[j][i] for j in range(len(board))]
+    if len(set(column)) == 1 and board[0][i]!='':
+      return board[0][i]
+
+  #check diagonals
+  top_left_to_bottom_right = [board[i][i] for i in range(len(board))]
+  if len(set(top_left_to_bottom_right)) == 1 and board[0][0] != '':
+    return board[0][0]
+
+  top_right_to_bottom_left = [board[i][len(board)-i-1] for i in range(len(board))]
+  if len(set(top_right_to_bottom_left)) == 1 and board[0][len(board)-1] != '':
+    return board[0][len(board)-1]
+
+  return None
 
 
 def other_player(player):
     """Given the character for a player, returns the other player."""
-    return "O"  # FIXME
+    return "O" if player == "X" else "X"
+
+def is_board_full(board):
+    for row in board:
+        for cell in row:
+            if cell is None:
+                return False
+    return True
